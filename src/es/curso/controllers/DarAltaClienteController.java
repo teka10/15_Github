5cc8f4dc45e8698496dc99bf19d3be3404b883bb
Usuario <Usuario@aula2-PC15>
2015-05-22 11:55:44 +0200

    Add darAltaClienteControllerEjb

--- src/es/curso/controllers/DarAltaClienteController.java
+++ src/es/curso/controllers/DarAltaClienteController.java
@@ -2,6 +2,7 @@ package es.curso.controllers;
 
 import es.curso.model.entity.Cliente;
 
-public interface DarAltaClienteController {
+public abstract interface DarAltaClienteController {
 	public void agregar(Cliente cliente);
+	
 }
---
+++ src/es/curso/controllers/ejb/DarAltaClienteControllerEjb.java
@@ -0,0 +1,18 @@
+package es.curso.controllers.ejb;
+
+import es.curso.controllers.DarAltaClienteController;
+import es.curso.model.entity.Cliente;
+
+public class DarAltaClienteControllerEjb implements DarAltaClienteController{
+
+	@Override
+	public void agregar(Cliente cliente) {
+		// lógica del negoci para agregar un cliente
+		// 1. verificar datos
+		// 2. Añadir cliente --> llamar a la capa DAO para q haga el alta
+		// 3. Enviar email informativo al comercial
+		// 4. Enviar email al cliente
+		
+	}
+
+}
