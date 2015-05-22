9ae8a3c89d38668b0dfc8c558ddce7766d66b593
Usuario <Usuario@aula2-PC15>
2015-05-22 09:52:02 +0200

    update clase cliente

--- src/es/curso/model/entity/Cliente.java
+++ src/es/curso/model/entity/Cliente.java
@@ -1,5 +1,76 @@
 package es.curso.model.entity;
 
 public class Cliente {
+	private int id;
+	private String name;
+	private String apellido;
+	
+	public Cliente() {
+		super();
+	}
+	public Cliente(int id, String name, String apellido) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.apellido = apellido;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", name=" + name + ", apellido="
+				+ apellido + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	//sirve para comparar objetos (por sus atributos), no sólo sus posiciones de memoria
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
 	
 }
